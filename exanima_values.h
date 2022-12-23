853dd22761a74875fb6b19d317d0a2a60d330586
EAM0719 <elijah.muron@yahoo.com>
2022-12-22 21:55:29 -0500

    Commit project files
    This commit contains the project files I've been working with prior to pushing anything to a Git repo.

---
+++ exanima_values.h
@@ -0,0 +1,9 @@
+#pragma once
+//Compatible Exanima Version:
+#define version "0.8.4c"
+//Values:
+#define maxhealth			0x3E800000
+//Addresses:
+#define player_entity		0x6A879C
+#define health_offset		0xB34
+#define max_health_offset	0xB38
