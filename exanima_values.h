336abb2ae9c8a2514e432136fce85fe92fb4773a
EAM0719 <elijah.muron@yahoo.com>
2022-12-23 01:10:04 -0500

    Changing some formatting

--- exanima_values.h
+++ exanima_values.h
@@ -2,8 +2,8 @@
 //Compatible Exanima Version:
 #define version "0.8.4c"
 //Values:
-#define maxhealth			0x3E800000
+#define maxhealth 0x3E800000
 //Addresses:
-#define player_entity		0x6A879C
-#define health_offset		0xB34
-#define max_health_offset	0xB38
+#define player_entity 0x6A879C
+#define health_offset 0xB34
+#define max_health_offset 0xB38
