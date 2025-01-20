32fd0f8be64596a4a34d1e7f4b56504835a85a7a
EAM0719 <elijah.muron@yahoo.com>
2025-01-20 01:53:24 -0500

    updated & optimised some things.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # E-Mortal
-Exanima immortal is a super simple tool was made more for testing purposes, rather than just being a "cheat".
+Exanima imMortal is a super simple tool was made more for testing purposes, rather than just being a "cheat".
 If you're trying to mod the game in anyway and need to stay alive, mess around with SIR, etc. This tool can help you with that!
 
 You can finally give SIR the hug he deserves!!!
--- exanima_values.hpp
+++ exanima_values.hpp
@@ -1,9 +1,11 @@
 #pragma once
+
 //Compatible Exanima Version:
-#define version "0.8.4c"
+#define version "0.9.0"
+
 //Values:
-#define maxhealth 0x3E800000
+#define maxhealth 0x3E8000003E800000 // The first 4 bytes is the max health, but I'm repeating it for the purpose of writing in two places at once with one function call.
+
 //Addresses:
-#define player_entity 0x6A879C
-#define health_offset 0xB34
-#define max_health_offset 0xB38
+#define player_entity 0x0071E7B4
+#define health_offset 0xB30 // The max health offset directly follows this.
